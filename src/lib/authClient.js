4e52791bfb7c062d7e7c2e689e83e07de10f750a
yusufnuru <yusufnuru2002@gmail.com>
2026-05-08 16:11:02 +0800

    fix: update authClient baseURL to include '/auth' path

--- src/lib/authClient.js
+++ src/lib/authClient.js
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react";
 import { adminClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: `${process.env.REACT_APP_API_URL}/auth`,
   plugins: [adminClient()],
 });
 
